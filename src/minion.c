182428bea488b3578927865cfe4ebaf4a803b48e
Patric Mueller <bhaak@gmx.net>
2009-02-22 17:27:45 +0000

    Bribe money for demons increased (from SporkHack)

--- src/minion.c
+++ src/minion.c
@@ -164,15 +164,25 @@ register struct monst *mtmp;
 	    if (!tele_restrict(mtmp)) (void) rloc(mtmp, FALSE);
 	    return(1);
 	}
-#ifndef GOLDOBJ
-	cash = u.ugold;
-#else
-	cash = money_cnt(invent);
-#endif
+
+	/* This isn't _that_ much better than the old way, but it removes
+	 * the trivial case of people being able to bribe demons with 
+	 * 10 gold pieces to bypass him.  You can still carry lots of gold,
+	 * of course, but at least now you have to lug it with you. */
+	do {
+		cash = rnd(15000) + 5000;
+
 	demand = (cash * (rnd(80) + 20 * Athome)) /
 	    (100 * (1 + (sgn(u.ualign.type) == sgn(mtmp->data->maligntyp))));
+	} while (demand < 3000);
 
-	if (!demand) {		/* you have no gold */
+	if (
+#ifndef GOLDOBJ
+			u.ugold == 0
+#else
+			money_cnt(invent) == 0
+#endif
+			) {						  /* you have no gold */
 	    mtmp->mpeaceful = 0;
 	    set_malign(mtmp);
 	    return 0;
@@ -181,7 +191,13 @@ register struct monst *mtmp;
 	       has the Amulet, preventing monster from being satisified
 	       and removed from the game (along with said Amulet...) */
 	    if (mon_has_amulet(mtmp))
-		demand = cash + (long)rn1(1000,40);
+			demand = 
+#ifndef GOLDOBJ
+				u.ugold
+#else
+				money_cnt(invent)
+#endif
+				+ (long)rn1(10000,40);
 
 	    pline("%s demands %ld %s for safe passage.",
 		  Amonnam(mtmp), demand, currency(demand));
