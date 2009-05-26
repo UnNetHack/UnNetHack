254229efdfa7a514020c4e24d3c69f8fe7743477
Patric Mueller <bhaak@gmx.net>
2009-05-26 14:32:18 +0000

    Increase patchlevel

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -9,7 +9,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL	1
+#define PATCHLEVEL	2
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
