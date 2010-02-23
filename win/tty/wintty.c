89cdfb854bc95d942b613e744de5e434752b7b73
Patric Mueller <bhaak@gmx.net>
2010-02-23 19:19:17 +0000

    Fix: Showing multiple items in inversed governed by use_inverse

--- win/tty/wintty.c
+++ win/tty/wintty.c
@@ -2560,7 +2560,7 @@ tty_print_glyph(window, x, y, glyph)
     /* must be after color check; term_end_color may turn off inverse too */
     if (((special & MG_PET) && iflags.hilite_pet) ||
 	((special & MG_DETECT) && iflags.use_inverse) ||
-	((special & MG_INVERSE))) {
+	((special & MG_INVERSE) && iflags.use_inverse)) {
 	term_start_attr(ATR_INVERSE);
 	reverse_on = TRUE;
     }
