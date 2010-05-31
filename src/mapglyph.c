90bac671e68b75008da2a070ab4dc0da06ea04d8
Patric Mueller <bhaak@gmx.net>
2010-05-31 21:08:03 +0000

    Fix: inverse video for stacks of items with corpses on top

--- src/mapglyph.c
+++ src/mapglyph.c
@@ -161,6 +161,11 @@ unsigned *ospecial;
 #endif
 	    mon_color(offset);
 	    special |= MG_RIDDEN;
+	    /* use inverse video for multiple items */
+	    if (level.objects[x][y] &&
+	        level.objects[x][y]->nexthere) {
+		special |= MG_INVERSE;
+	    }
     } else if ((offset = (glyph - GLYPH_BODY_OFF)) >= 0) {	/* a corpse */
 	ch = oc_syms[(int)objects[CORPSE].oc_class];
 #ifdef ROGUE_COLOR
