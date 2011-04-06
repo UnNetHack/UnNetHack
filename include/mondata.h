a22f125f6a4ba105b207fd3d55fa4f0d0779818a
Patric Mueller <bhaak@gmx.net>
2011-04-06 19:34:46 +0000

    Flesh golem corpses edible for vampires (fixed ticket:59)

--- include/mondata.h
+++ include/mondata.h
@@ -206,7 +206,7 @@
 
 /* For vampires */
 #define has_blood(ptr)		(!vegetarian(ptr) && \
-				   (ptr)->mlet != S_GOLEM && \
+				   ((ptr)->mlet != S_GOLEM || (ptr) == &mons[PM_FLESH_GOLEM]) && \
 				   (!is_undead(ptr) || is_vampire(ptr)))
 
 #define befriend_with_obj(ptr, obj) ((obj)->oclass == FOOD_CLASS && \
