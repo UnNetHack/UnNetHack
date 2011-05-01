1774d5b85a632d833fbab246747d0d8ca0332e00
Patric Mueller <bhaak@gmx.net>
2011-05-01 14:03:43 +0000

    Put scroll of mail glyph at correct place after new scrolls were added

--- win/share/tilemap.c
+++ win/share/tilemap.c
@@ -93,7 +93,7 @@ struct conditionals {
 	 * don't know what a slime mold should look like when renamed anyway
 	 */
 #ifndef MAIL
-	{ OBJ_GLYPH, SCR_STINKING_CLOUD+4, "stamped / mail" },
+	{ OBJ_GLYPH, SCR_STINKING_CLOUD+21, "stamped / mail" },
 #endif
 	{ 0, 0, 0}
 };
