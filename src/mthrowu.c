7f2ffc6472744cf021582a5ea0dbcb84022e1ce4
Patric Mueller <bhaak@gmx.net>
2011-07-08 17:11:41 +0000

    Fixed breaking of pacifist conduct when killing a monster with an unknown rolling boulder trap

--- src/mthrowu.c
+++ src/mthrowu.c
@@ -227,7 +227,7 @@ boolean verbose;  /* give message(s) even when you can't see what happened */
 			? "destroyed" : "killed");
 		/* don't blame hero for unknown rolling boulder trap */
 		if (!flags.mon_moving &&
-		    (otmp->otyp != BOULDER || range >= 0 || !otmp->otrapped))
+		    (otmp->otyp != BOULDER || range >= 0 || otmp->otrapped))
 		    xkilled(mtmp,0);
 		else mondied(mtmp);
 	    }
