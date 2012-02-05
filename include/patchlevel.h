e5b35f3291bad8b66ff6de455076876e2db67f9a
Patric Mueller <bhaak@gmx.net>
2012-02-05 12:12:03 +0000

    Version bump to 3.6.1

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -9,7 +9,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL	0
+#define PATCHLEVEL	1
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
@@ -49,7 +49,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-/*#define VERSION_COMPATIBILITY 0x03060000L*/	/* 3.6.0-e0 */
+#define VERSION_COMPATIBILITY 0x03060000L	/* 3.6.0-e0 */
 
 
 /*****************************************************************************/
