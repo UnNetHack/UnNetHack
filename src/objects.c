98bdcfb8a2f45e150c8337a9ef7c048a8f464ce8
Patric Mueller <bhaak@gmx.net>
2012-03-31 21:18:58 +0000

    Reduce AC of dragon scales to 1 and dragon scale mails to 5

--- src/objects.c
+++ src/objects.c
@@ -333,38 +333,38 @@ HELM("helm of telepathy", "visored helmet",
 	ARMOR(name,desc,0,mgc,1,power,0,5,40,cost,ac,0,ARM_SUIT,DRAGON_HIDE,color)
 /* 3.4.1: dragon scale mail reclassified as "magic" since magic is
    needed to create them */
-DRGN_ARMR("magic dragon scale mail", "tatzelworm scale mail", 1, ANTIMAGIC,  1200, 1, CLR_GRAY),
-DRGN_ARMR("reflecting dragon scale mail", "amphitere scale mail", 1, REFLECTING, 1200, 1, DRAGON_SILVER),
+DRGN_ARMR("magic dragon scale mail", "tatzelworm scale mail", 1, ANTIMAGIC,  1200, 5, CLR_GRAY),
+DRGN_ARMR("reflecting dragon scale mail", "amphitere scale mail", 1, REFLECTING, 1200, 5, DRAGON_SILVER),
 #if 0	/* DEFERRED */
-DRGN_ARMR("shimmering dragon scale mail", 1, DISPLACED, 1200, 1, CLR_CYAN),
+DRGN_ARMR("shimmering dragon scale mail", 1, DISPLACED, 1200, 5, CLR_CYAN),
 #endif
-DRGN_ARMR("fire dragon scale mail", "draken scale mail", 1, FIRE_RES,    900, 1, CLR_RED),
-DRGN_ARMR("ice dragon scale mail", "lindworm scale mail", 1, COLD_RES,    900, 1, CLR_WHITE),
-DRGN_ARMR("sleep dragon scale mail", "sarkany scale mail", 1, SLEEP_RES,   900, 1, CLR_ORANGE),
-DRGN_ARMR("disintegration dragon scale mail", "sirrush scale mail", 1, DISINT_RES, 1200, 1, CLR_BLACK),
-DRGN_ARMR("electric dragon scale mail", "leviathan scale mail", 1, SHOCK_RES,   900, 1, CLR_BLUE),
-DRGN_ARMR("poison dragon scale mail", "wyvern scale mail", 1, POISON_RES,  900, 1, CLR_GREEN),
-DRGN_ARMR("stone dragon scale mail", "gold dragon scale mail", 1, STONE_RES,   900, 1, HI_GOLD),
-DRGN_ARMR("acid dragon scale mail", "guivre scale mail", 1, ACID_RES,    900, 1, CLR_YELLOW),
-DRGN_ARMR("chromatic dragon scale mail", "chromatic dragon scale mail", 1, POISON_RES, 2400, 1, CLR_MAGENTA), /* intrinsics are set manually */
+DRGN_ARMR("fire dragon scale mail", "draken scale mail", 1, FIRE_RES,    900, 5, CLR_RED),
+DRGN_ARMR("ice dragon scale mail", "lindworm scale mail", 1, COLD_RES,    900, 5, CLR_WHITE),
+DRGN_ARMR("sleep dragon scale mail", "sarkany scale mail", 1, SLEEP_RES,   900, 5, CLR_ORANGE),
+DRGN_ARMR("disintegration dragon scale mail", "sirrush scale mail", 1, DISINT_RES, 1200, 5, CLR_BLACK),
+DRGN_ARMR("electric dragon scale mail", "leviathan scale mail", 1, SHOCK_RES,   900, 5, CLR_BLUE),
+DRGN_ARMR("poison dragon scale mail", "wyvern scale mail", 1, POISON_RES,  900, 5, CLR_GREEN),
+DRGN_ARMR("stone dragon scale mail", "gold dragon scale mail", 1, STONE_RES,   900, 5, HI_GOLD),
+DRGN_ARMR("acid dragon scale mail", "guivre scale mail", 1, ACID_RES,    900, 5, CLR_YELLOW),
+DRGN_ARMR("chromatic dragon scale mail", "chromatic dragon scale mail", 1, POISON_RES, 2400, 5, CLR_MAGENTA), /* intrinsics are set manually */
 
 /* For now, only dragons leave these. */
 /* 3.4.1: dragon scales left classified as "non-magic"; they confer
    magical properties but are produced "naturally" */
-DRGN_ARMR("magic dragon scales", "tatzelworm scales", 0, ANTIMAGIC,  700, 7, CLR_GRAY),
-DRGN_ARMR("reflecting dragon scales", "amphitere scales", 0, REFLECTING, 700, 7, DRAGON_SILVER),
+DRGN_ARMR("magic dragon scales", "tatzelworm scales", 0, ANTIMAGIC,  700, 9, CLR_GRAY),
+DRGN_ARMR("reflecting dragon scales", "amphitere scales", 0, REFLECTING, 700, 9, DRAGON_SILVER),
 #if 0	/* DEFERRED */
-DRGN_ARMR("shimmering dragon scales", 0, DISPLACED,  700, 7, CLR_CYAN),
+DRGN_ARMR("shimmering dragon scales", 0, DISPLACED,  700, 9, CLR_CYAN),
 #endif
-DRGN_ARMR("fire dragon scales", "draken scales", 0, FIRE_RES,   500, 7, CLR_RED),
-DRGN_ARMR("ice dragon scales", "lindworm scales", 0, COLD_RES,   500, 7, CLR_WHITE),
-DRGN_ARMR("sleep dragon scales", "sarkany scales", 0, SLEEP_RES,  500, 7, CLR_ORANGE),
-DRGN_ARMR("disintegration dragon scales", "sirrush scales",  0, DISINT_RES, 700, 7, CLR_BLACK),
-DRGN_ARMR("electric dragon scales", "leviathan scales", 0, SHOCK_RES,  500, 7, CLR_BLUE),
-DRGN_ARMR("poison dragon scales", "wyvern scales", 0, POISON_RES, 500, 7, CLR_GREEN),
-DRGN_ARMR("stone dragon scales", "gold dragon scales", 0, STONE_RES,  500, 7, HI_GOLD),
-DRGN_ARMR("acid dragon scales", "guivre scales", 0, ACID_RES,   500, 7, CLR_YELLOW),
-DRGN_ARMR("chromatic dragon scales", "chromatic dragon scales", 0, 0, 1500, 7, CLR_MAGENTA), /* intrinsics are set manually */
+DRGN_ARMR("fire dragon scales", "draken scales", 0, FIRE_RES,   500, 9, CLR_RED),
+DRGN_ARMR("ice dragon scales", "lindworm scales", 0, COLD_RES,   500, 9, CLR_WHITE),
+DRGN_ARMR("sleep dragon scales", "sarkany scales", 0, SLEEP_RES,  500, 9, CLR_ORANGE),
+DRGN_ARMR("disintegration dragon scales", "sirrush scales",  0, DISINT_RES, 700, 9, CLR_BLACK),
+DRGN_ARMR("electric dragon scales", "leviathan scales", 0, SHOCK_RES,  500, 9, CLR_BLUE),
+DRGN_ARMR("poison dragon scales", "wyvern scales", 0, POISON_RES, 500, 9, CLR_GREEN),
+DRGN_ARMR("stone dragon scales", "gold dragon scales", 0, STONE_RES,  500, 9, HI_GOLD),
+DRGN_ARMR("acid dragon scales", "guivre scales", 0, ACID_RES,   500, 9, CLR_YELLOW),
+DRGN_ARMR("chromatic dragon scales", "chromatic dragon scales", 0, 0, 1500, 9, CLR_MAGENTA), /* intrinsics are set manually */
 #undef DRGN_ARMR
 
 ARMOR("plate mail", (char *)0,
