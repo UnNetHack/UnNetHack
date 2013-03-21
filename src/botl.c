7c46d5ccbbdd01ccf03ece3eda82ebbdd0c409fa
guest41 <guestfortyone@gmail.com>
2013-03-21 21:09:26 +0000

    When standing on Elbereth, show it in statuscolors

--- src/botl.c
+++ src/botl.c
@@ -558,6 +558,12 @@ bot2()
 #else
 		Sprintf(nb = eos(nb), " %s", enc_stat[cap]);
 #endif
+	if(!Blind && sengr_at("Elbereth", u.ux, u.uy))
+#if defined(STATUS_COLORS) && defined(TEXTCOLOR)
+	     	add_colored_text("Elbereth", newbot2);
+#else
+		Strcat(nb = eos(nb), " Elbereth");
+#endif
 #ifdef DUMP_LOG
 }
 STATIC_OVL void
