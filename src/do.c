2b65421fd47c900b8162a84c8813ba79156d9c67
Patric Mueller <bhaak@gmx.net>
2013-04-01 21:03:13 +0000

    Fix "You hear The knife tumbles downwards."
    Without this fix, this kind of message appears when dropping an item
    over a pit while blind, complete with bad capitalization of "The" and
    "tumbles" instead of "tumble".  Found by boxo/Box_ in the #unnethack IRC
    channel on FreeNode.
    
    Fix by tung:
    https://github.com/tung/NitroHack/commit/5d40e9f86486141df827e2afef78e992c4aac01b

--- src/do.c
+++ src/do.c
@@ -236,8 +236,7 @@ const char *verb;
 			(t->ttyp==PIT || t->ttyp==SPIKED_PIT)) {
 		/* you escaped a pit and are standing on the precipice */
 		if (Blind && flags.soundok)
-			You_hear("%s %s downwards.",
-				The(xname(obj)), otense(obj, "tumble"));
+			You_hear("%s tumble downwards.", the(xname(obj)));
 		else
 			pline("%s %s into %s pit.",
 				The(xname(obj)), otense(obj, "tumble"),
