255ff6e6d677c67376d4fe6e084427955cc6ad47
Patric Mueller <bhaak@gmx.net>
2013-06-15 17:16:00 +0000

    Set VERSION_COMPATIBILITY to 5.0.0

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -49,7 +49,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-/* #define VERSION_COMPATIBILITY 0x05000000L */	/* 5.0.0-e0 */
+#define VERSION_COMPATIBILITY 0x05000000L	/* 5.0.0-e0 */
 
 
 /*****************************************************************************/
