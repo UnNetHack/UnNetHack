4ebd519737cd7fb9d9c405ee0000de1032c9410d
Patric Mueller <bhaak@gmx.net>
2013-10-09 07:19:48 +0000

    Bump patchlevel to 5.0.4

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -9,7 +9,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL	3
+#define PATCHLEVEL	4
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
@@ -49,7 +49,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x05000000L	/* 5.0.0-e0 */
+/* #define VERSION_COMPATIBILITY 0x05000000L */	/* 5.0.0-e0 */
 
 
 /*****************************************************************************/
