19fad45e02571dddd8c44a2cb790d6a870f58437
Patric Mueller <bhaak@gmx.net>
2013-11-19 11:28:26 +0000

    Bump patchlevel to 5.0.5

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -9,7 +9,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL	4
+#define PATCHLEVEL	5
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
