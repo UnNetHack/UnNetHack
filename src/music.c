15871493880886d22292e154899ca278663ef02b
Tung Nguyen <tungtn3@gmail.com>
2014-05-09 00:25:50 +1000

    Give message when fire/frost horn/drum of earth is identified by use
    e.g. "You start playing the horn.  It must be a frost horn!"

--- src/music.c
+++ src/music.c
@@ -416,7 +416,7 @@ struct obj *instr;
 		    buzz((instr->otyp == FROST_HORN) ? AD_COLD-1 : AD_FIRE-1,
 			 rn1(6,6), u.ux, u.uy, u.dx, u.dy);
 		}
-		makeknown(instr->otyp);
+		makeknown_msg(instr->otyp);
 		break;
 	    } /* else FALLTHRU */
 	case TOOLED_HORN:		/* Awaken or scare monsters */
@@ -455,7 +455,7 @@ struct obj *instr;
 		do_earthquake((u.ulevel - 1) / 3 + 1);
 		/* shake up monsters in a much larger radius... */
 		awaken_monsters(ROWNO * COLNO);
-		makeknown(DRUM_OF_EARTHQUAKE);
+		makeknown_msg(DRUM_OF_EARTHQUAKE);
 		break;
 	    } /* else FALLTHRU */
 	case LEATHER_DRUM:		/* Awaken monsters */
