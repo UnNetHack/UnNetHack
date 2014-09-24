f06343f4411124a7c4a86f7565eb4cfb7f0284e8
Patric Mueller <bhaak@gmx.net>
2014-09-25 00:27:10 +0200

    remove unneeded floating point random functions

--- src/rnd_isaac.c
+++ src/rnd_isaac.c
@@ -1,10 +1,8 @@
 /*Written by Timothy B. Terriberry (tterribe@xiph.org) 1999-2009.
-  CC0 (Public domain) - see LICENSE file for details
+  CC0 (Public domain) - see http://creativecommons.org/publicdomain/zero/1.0/ for details
   Based on the public domain implementation by Robert J. Jenkins Jr.*/
-#include <float.h>
 #include <math.h>
 #include <string.h>
-#include <ccan/ilog/ilog.h>
 #include "isaac.h"
 
 
@@ -147,113 +145,4 @@ uint32_t isaac_next_uint(isaac_ctx *_ctx,uint32_t _n){
   }
   while(((d+_n-1)&ISAAC_MASK)<d);
   return v;
-}
-
-/*Returns a uniform random float.
-  The expected value is within FLT_MIN (e.g., 1E-37) of 0.5.
-  _bits: An initial set of random bits.
-  _base: This should be -(the number of bits in _bits), up to -32.
-  Return: A float uniformly distributed between 0 (inclusive) and 1
-           (exclusive).
-          The average value was measured over 2**32 samples to be
-           0.50000037448772916.*/
-static float isaac_float_bits(isaac_ctx *_ctx,uint32_t _bits,int _base){
-  float ret;
-  int   nbits_needed;
-  while(!_bits){
-    if(_base+FLT_MANT_DIG<FLT_MIN_EXP)return 0;
-    _base-=32;
-    _bits=isaac_next_uint32(_ctx);
-  }
-  /*Note: This could also be determined with frexp(), for a slightly more
-     portable solution, but that takes twice as long, and one has to worry
-     about rounding effects, which can over-estimate the exponent when given
-     FLT_MANT_DIG+1 consecutive one bits.
-    Even the fallback C implementation of ILOGNZ_32() yields an implementation
-     25% faster than the frexp() method.*/
-  nbits_needed=FLT_MANT_DIG-ilog32_nz(_bits);
-#if FLT_MANT_DIG>32
-  ret=ldexpf((float)_bits,_base);
-# if FLT_MANT_DIG>65
-  while(32-nbits_needed<0){
-# else
-  if(32-nbits_needed<0){
-# endif
-    _base-=32;
-    nbits_needed-=32;
-    ret+=ldexpf((float)isaac_next_uint32(_ctx),_base);
-  }
-  _bits=isaac_next_uint32(_ctx)>>32-nbits_needed;
-  ret+=ldexpf((float)_bits,_base-nbits_needed);
-#else
-  if(nbits_needed>0){
-    _bits=_bits<<nbits_needed|isaac_next_uint32(_ctx)>>(32-nbits_needed);
-  }
-# if FLT_MANT_DIG<32
-  else _bits>>=-nbits_needed;
-# endif
-  ret=ldexpf((float)_bits,_base-nbits_needed);
-#endif
-  return ret;
-}
-
-float isaac_next_float(isaac_ctx *_ctx){
-  return isaac_float_bits(_ctx,0,0);
-}
-
-float isaac_next_signed_float(isaac_ctx *_ctx){
-  uint32_t bits;
-  bits=isaac_next_uint32(_ctx);
-  return (1|-((int)bits&1))*isaac_float_bits(_ctx,bits>>1,-31);
-}
-
-/*Returns a uniform random double.
-  _bits: An initial set of random bits.
-  _base: This should be -(the number of bits in _bits), up to -32.
-  Return: A double uniformly distributed between 0 (inclusive) and 1
-           (exclusive).
-          The average value was measured over 2**32 samples to be
-           0.500006289408060911*/
-static double isaac_double_bits(isaac_ctx *_ctx,uint32_t _bits,int _base){
-  double ret;
-  int    nbits_needed;
-  while(!_bits){
-    if(_base+DBL_MANT_DIG<DBL_MIN_EXP)return 0;
-    _base-=32;
-    _bits=isaac_next_uint32(_ctx);
-  }
-  nbits_needed=DBL_MANT_DIG-ilog32_nz(_bits);
-#if DBL_MANT_DIG>32
-  ret=ldexp((double)_bits,_base);
-# if DBL_MANT_DIG>65
-  while(32-nbits_needed<0){
-# else
-  if(32-nbits_needed<0){
-# endif
-    _base-=32;
-    nbits_needed-=32;
-    ret+=ldexp((double)isaac_next_uint32(_ctx),_base);
-  }
-  _bits=isaac_next_uint32(_ctx)>>(32-nbits_needed);
-  ret+=ldexp((double)_bits,_base-nbits_needed);
-#else
-  if(nbits_needed>0){
-    _bits=_bits<<nbits_needed|isaac_next_uint32(_ctx)>>32-nbits_needed;
-  }
-# if DBL_MANT_DIG<32
-  else _bits>>=-nbits_needed;
-# endif
-  ret=ldexp((double)_bits,exp-DBL_MANT_DIG);
-#endif
-  return ret;
-}
-
-double isaac_next_double(isaac_ctx *_ctx){
-  return isaac_double_bits(_ctx,0,0);
-}
-
-double isaac_next_signed_double(isaac_ctx *_ctx){
-  uint32_t bits;
-  bits=isaac_next_uint32(_ctx);
-  return (1|-((int)bits&1))*isaac_double_bits(_ctx,bits>>1,-31);
 }
