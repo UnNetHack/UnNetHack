3bb732f184e5f30bb60b478cef089cf078152579
Patric Mueller <bhaak@gmx.net>
2015-02-21 15:41:24 +0100

    Fixed crash with a configured nameempty option (found and fixed by tertium)

--- src/zap.c
+++ src/zap.c
@@ -1913,7 +1913,7 @@ dozap()
 	if(!zappable(obj)) {
 	    pline("%s", nothing_happens);
 	    /* if nameempty is set, then #name unnamed known empty wands */
-	    if (iflags.nameempty && !obj->onamelth) oname(obj, iflags.nameempty);
+	    if (iflags.nameempty && !obj->onamelth) obj = oname(obj, iflags.nameempty);
 	}
 	/* PM 2008-04-16: 50% chance of blowing up, if zapped 20 times.
 	 * Same probability as in muse.c precheck() for monsters */
