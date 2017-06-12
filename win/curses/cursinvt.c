k21971/GruntHack

Fix a bug where menuglyphs would screw up sidebar colors if menucolors was disabled

win/curses/cursinvt.c
@@ -85,10 +85,10 @@ curses_add_inv(int y, int glyph, CHAR_P accelerator, attr_t attr,
         int curletter = 0;
         mapglyph_obj(glyph, &curletter, &color, &dummy,
                      u.ux, u.uy, identifier->a_obj);
-        attr = curses_color_attr(color, 0);
-        wattron(win, attr);
+        attr_t glyph = curses_color_attr(color, 0);
+        wattron(win, glyph);
         wprintw(win, "%c ", curletter);
-        wattroff(win, attr);
+        wattroff(win, glyph);
     }
 
 #ifdef MENU_COLOR

UnNetHack/UnNetHack

win/curses/cursinvt.c
@@ -85,10 +85,10 @@ curses_add_inv(int y, int glyph, CHAR_P accelerator, attr_t attr,
         int symbol = 0;
         mapglyph(glyph, &symbol, &color, &dummy,
                  0, 0);
-        attr = curses_color_attr(color, 0);
-        wattron(win, attr);
+        attr_t glyphclr = curses_color_attr(color, 0);
+        wattron(win, glyphclr);
         wprintw(win, "%c ", symbol);
-        wattroff(win, attr);
+        wattroff(win, glyphclr);
     }
 
 #ifdef MENU_COLOR