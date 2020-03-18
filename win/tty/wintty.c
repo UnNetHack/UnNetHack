c0f8d4c355f8dc9fb5e8d88d93473cca412edaac
Patric Mueller <bhaak@gmx.net>
2020-03-18 02:01:59 +0100

    Optimized placement of menu windows in TTY port
    Default placement of menu windows was right aligning them with the
    terminals width.
    
    This resulted in menu windows being way over on the right side if you
    have a very wide terminal or if the terminal is wider than 255
    characters, menu windows being left aligned (like the win_edge option)
    due of a number overflow bug in calculating the x offset (C343-439).
    
    This commit changes the calculation of the x offset such that it will
    put the menu window right next to the map if there is enough horizontal
    space. Otherwise it overlaps the window with the map as before.

--- win/tty/wintty.c
+++ win/tty/wintty.c
@@ -1872,8 +1872,8 @@ tty_display_nhwindow(window, blocking)
     case NHW_MENU:
 	cw->active = 1;
 	/* avoid converting to uchar before calculations are finished */
-	cw->offx = (uchar) (int)
-	    max((int) 10, (int) (ttyDisplay->cols - cw->maxcol - 1));
+
+    cw->offx = min(COLNO-1, ttyDisplay->cols - cw->maxcol - 1);
 	if(cw->type == NHW_MENU
 #ifdef WIN_EDGE
 	    || iflags.win_edge
@@ -1882,7 +1882,7 @@ tty_display_nhwindow(window, blocking)
 	    cw->offy = 0;
 	if(ttyDisplay->toplin == 1)
 	    tty_display_nhwindow(WIN_MESSAGE, TRUE);
-	if(cw->offx == 10 || cw->maxrow >= (int) ttyDisplay->rows
+    if (cw->offx < 0 || cw->maxrow >= ttyDisplay->rows
 #ifdef WIN_EDGE
 	    || iflags.win_edge
 #endif
