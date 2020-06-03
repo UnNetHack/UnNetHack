e79e4a2d4bf006ee94dd7eb08b0f52f736abc41d
Patric Mueller <bhaak@gmx.net>
2020-06-03 23:28:43 +0200

    Ensure gold being first in flags.inv_order if missing from packorder

--- src/options.c
+++ src/options.c
@@ -1022,6 +1022,9 @@ char *op;
     char *sp, buf[BUFSZ];
 
     num = 0;
+    if (!index(op, GOLD_SYM)) {
+        buf[num++] = COIN_CLASS;
+    }
 
     for (sp = op; *sp; sp++) {
         oc_sym = def_char_to_objclass(*sp);
