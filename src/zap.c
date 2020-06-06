924732a76dd9cb3d5d36f07ad84b4a75991504ca
Patric Mueller <bhaak@gmx.net>
2020-06-06 21:37:47 +0200

    Prevent Cthulhu's inventory from being shown when probed

--- src/zap.c
+++ src/zap.c
@@ -531,6 +531,10 @@ struct monst *mtmp;
 
     mstatusline(mtmp);
     if (notonhead) return;  /* don't show minvent for long worm tail */
+    if (mtmp->data == &mons[PM_CTHULHU]) {
+        /* Cthulhu keeps his secrets */
+        return;
+    }
 
     if (mtmp->minvent) {
         for (otmp = mtmp->minvent; otmp; otmp = otmp->nobj) {
