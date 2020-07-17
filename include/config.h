d87ff53a7e587d317720c8c8c4deaf796bc14ec7
Patric Mueller <bhaak@gmx.net>
2020-07-17 12:29:21 +0200

    Autoconf automatically select correct regex engine wrapper

--- include/config.h
+++ include/config.h
@@ -421,10 +421,7 @@ typedef long glyph_t;
     defined(CURSES_GRAPHICS)
 # define MENU_COLOR
 /*# define MENU_COLOR_REGEX*/
-/*# define MENU_COLOR_REGEX_POSIX */
-/* if MENU_COLOR_REGEX is defined, use regular expressions (regex.h,
- * GNU specific functions by default, POSIX functions with
- * MENU_COLOR_REGEX_POSIX).
+/* if MENU_COLOR_REGEX is defined, use regular expressions (regex.h)
  * otherwise use pmatch() to match menu color lines.
  * pmatch() provides basic globbing: '*' and '?' wildcards.
  */
