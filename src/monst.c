2a16aa257fe3974eb1b30c3e3696e7f23768451a
Pasi Kallinen <paxed@alt.org>
2020-09-30 18:59:09 +0300

    Make piranhas faster and more bitey
    Piranhas are pretty bland, so make them faster and give them
    an extra bite attack.
    
    (via xNetHack)

--- src/monst.c
+++ src/monst.c
@@ -3095,8 +3095,8 @@ struct permonst _mons2[] = {
         M1_SWIM|M1_AMPHIBIOUS|M1_SLITHY|M1_NOLIMBS|M1_NOTAKE|M1_POIS,
         M2_HOSTILE, 0, CLR_BLUE),
     MON("piranha", S_EEL,
-        LVL(5, 12, 4, 0, 0), (G_GENO|G_NOGEN|G_SGROUP),
-        A(ATTK(AT_BITE, AD_PHYS, 2, 6), NO_ATTK,
+        LVL(5, 18, 4, 0, 0), (G_GENO | G_NOGEN | G_SGROUP),
+        A(ATTK(AT_BITE, AD_PHYS, 2, 6), ATTK(AT_BITE, AD_PHYS, 2, 6),
           NO_ATTK, NO_ATTK, NO_ATTK, NO_ATTK),
         SIZ(60, 30, 0, MS_SILENT, MZ_SMALL), 0, 0,
         M1_SWIM|M1_AMPHIBIOUS|M1_ANIMAL|M1_SLITHY|M1_NOLIMBS|
