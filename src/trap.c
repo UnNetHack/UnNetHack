3338236ff50c05d850e839ff62c9b4bd58458f06
Patric Mueller <bhaak@gmx.net>
2021-04-20 13:38:34 +0200

    Hallucinatory liquids for water damage messages

--- src/trap.c
+++ src/trap.c
@@ -3732,13 +3732,13 @@ boolean force;
     } else if (Is_container(obj) &&
                (!Is_waterproof_container(obj) || (obj->cursed && !rn2(3)))) {
         if (carried(obj)) {
-            pline("Some water gets into your %s!", ostr);
+            pline("Some %s gets into your %s!", hliquid("water"), ostr);
         }
         water_damage_chain(obj->cobj, FALSE);
         return ER_DAMAGED; /* contents were damaged */
     } else if (Is_waterproof_container(obj)) {
         if (carried(obj)) {
-            pline_The("water slides right off your %s.", ostr);
+            pline_The("%s slides right off your %s.", hliquid("water"), ostr);
             makeknown(obj->otyp);
         }
         /* not actually damaged, but because we /didn't/ get the "water
