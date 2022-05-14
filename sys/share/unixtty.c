2db4b9dbaa53047f9bc0ff8e891597d64eb316da
Patric Mueller <bhaak@gmx.net>
2022-05-14 20:14:57 +0200

    More careful setting the terminal into raw mode
    According to the bug report, the previous code for setting the terminal
    into raw mode for UTF8 detection could have the side effect of
    terminating the login session and closing the running xterm due to the
    speed being set to 0.
    
    Closes #47

--- sys/share/unixtty.c
+++ sys/share/unixtty.c
@@ -463,7 +463,8 @@ check_utf8_console()
     }
 
     /* set minimal raw mode */
-    struct termios raw = { 0 };
+    struct termios raw = original;
+    cfmakeraw(&raw);
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 3; /* timeout of 0.3 seconds */
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
