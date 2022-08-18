73a66efbe5d71852edc5f9e73526f4b5bf98f629
Patric Mueller <bhaak@gmx.net>
2022-08-18 22:24:23 +0200

    Bump version number to 6.0.6

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -8,7 +8,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL  5
+#define PATCHLEVEL  6
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
@@ -48,7 +48,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x06000500L   /* 6.0.5-e0 */
+#define VERSION_COMPATIBILITY 0x06000600L   /* 6.0.6-e0 */
 
 
 /*****************************************************************************/
