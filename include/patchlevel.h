4e468840f749a71795c8f9d5a3b0e74ddc351b5b
Patric Mueller <bhaak@gmx.net>
2026-05-16 17:06:12 +0200

    Bump version number to 6.0.15

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -8,7 +8,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL  14
+#define PATCHLEVEL  15
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
